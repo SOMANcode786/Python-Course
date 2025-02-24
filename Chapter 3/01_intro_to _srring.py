357a696acdc754459be115e2ce7da70c9641fbc3
Muhammad Soman <somanamir43@gmail.com>
2025-02-24 23:03:23 +0500

    add new file

---
+++ Chapter 3/01_intro_to _srring.py
@@ -0,0 +1,15 @@
+name="Soman"   #double quote string
+b='soman' #single quote string
+b='''soman'''  #Triple quote string
+
+
+# string is immutable means agr hmm na aik dafa string bna di phir usy 
+#       change nahi kar sakhty you can update a string you make a new string 
+
+# to find the lenght of string use a method {  len()  }
+
+length=len(name)
+print("Length of the string is : ",length)
+
+
+nameshort=name
---
+++ Chapter 3/02_negative_slicing.py
@@ -0,0 +1,11 @@
+a="soman"
+
+print(a[0:2])   #start 0 till 1 excluded 2
+
+print(a[2:])   #start 2 till end
+
+print(a[:3])   #start 0 till 2 excluded
+
+print(a[-1])  #last element
+
+print(a[-2:])  #last two elements
