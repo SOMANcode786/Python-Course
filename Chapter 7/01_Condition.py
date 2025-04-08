62f90fcd8416bc2e59da32ed28bf64d15574fdb8
Muhammad Soman <somanamir43@gmail.com>
2025-04-09 00:31:23 +0500

    update a folder

---
+++ Chapter 7/01_Condition.py
@@ -0,0 +1,10 @@
+
+
+a=int(input("Enter your Age : "))
+
+if(a>=18):
+    print("You are above the age of consent ")
+    print("Good Morning")
+
+else:
+    print("you are below the age of consent")
